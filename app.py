de30f77983114f167ecc2e4ab6998bc44b4b644d
G B <gerardbouaziz@yahoo.fr>
2022-10-29 16:18:47 +0200

    new app

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ from sklearn.neighbors import NearestNeighbors
 app = Flask(__name__)
 
 #app.config["CLIENT_PDF"] = "./pdf_files"
+#app.config["CLIENT_PDF"] = "./pdf_files"
+
 app.config["CLIENT_PDF"] = "/tmp"
 
 ############################################################
